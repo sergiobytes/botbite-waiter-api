4b11bb895cebe178e247b7e8b569426e28f77b1c
Sergio Barreras <sergiobg.isc@gmail.com>
2025-10-05 23:27:46 -0600

    feat(users): add user activation and deactivation functionality with translations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,8 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { TranslationService } from '../common/services/translation.service';
 import { isUUID } from 'class-validator';
-import { RegsiterUserDto } from './dto/register-user';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { UserRoles } from './enums/user-roles';
 
 import * as argon2 from 'argon2';
 
@@ -18,7 +19,7 @@ export class UsersService {
     private readonly translationService: TranslationService,
   ) {}
 
-  async registerUser(registerUserDto: RegsiterUserDto, lang: string) {
+  async registerUser(registerUserDto: RegisterUserDto, lang: string) {
     const { email, password } = registerUserDto;
 
     const [userExists, hashedPassword] = await Promise.all([
@@ -47,6 +48,100 @@ export class UsersService {
       ),
     };
   }
+
+  async registerClient(registerUserDto: RegisterUserDto, lang: string) {
+    const { email, password } = registerUserDto;
+
+    const [userExists, hashedPassword] = await Promise.all([
+      this.findUserByTerm(email),
+      argon2.hash(password),
+    ]);
+
+    if (userExists) {
+      this.logger.warn(`User registration failed - User exists: ${email}`);
+      throw new BadRequestException(
+        this.translationService.translate('errors.user_exists', lang),
+      );
+    }
+
+    const user = this.userRepository.create({
+      email,
+      password: hashedPassword,
+      roles: [UserRoles.CLIENT],
+    });
+
+    await this.userRepository.save(user);
+
+    return {
+      message: this.translationService.translate(
+        'auth.registration_success',
+        lang,
+      ),
+    };
+  }
+
+  async activateUser(userId: string, lang: string) {
+    const user = await this.findUserByTerm(userId);
+
+    if (!user) {
+      this.logger.warn(`Activate user failed - User not found: ${userId}`);
+      throw new BadRequestException(
+        this.translationService.translate('errors.user_not_found', lang),
+      );
+    }
+
+    if (user.isActive) {
+      this.logger.warn(
+        `Activate user failed - User already active: ${user.email}`,
+      );
+      throw new BadRequestException(
+        this.translationService.translate('users.user_already_active', lang),
+      );
+    }
+
+    user.isActive = true;
+    await this.userRepository.save(user);
+    this.logger.log(`User activated: ${user.email}`);
+
+    return {
+      message: this.translationService.translate('users.user_activated', lang),
+    };
+  }
+
+  async deactivateUser(userId: string, lang: string) {
+    const user = await this.findUserByTerm(userId);
+
+    if (!user) {
+      this.logger.warn(`Activate user failed - User not found: ${userId}`);
+      throw new BadRequestException(
+        this.translationService.translate('errors.user_not_found', lang),
+      );
+    }
+
+    if (!user.isActive) {
+      this.logger.warn(
+        `Activate user failed - User already inactive: ${user.email}`,
+      );
+      throw new BadRequestException(
+        this.translationService.translate('users.user_already_inactive', lang),
+      );
+    }
+
+    user.isActive = false;
+    await this.userRepository.save(user);
+    this.logger.log(`User deactivated: ${user.email}`);
+
+    return {
+      message: this.translationService.translate(
+        'users.user_deactivated',
+        lang,
+      ),
+    };
+  }
+
+  // TODO: Obtener usuarios paginados con el role client y con filtros (email, activos)
+
+  // TODO: Agregar rol de administrador a un usuario que no tenga el rol de cliente y tiene que ser de un usuario activo y que ya sea administrador
 
   async findUserByTerm(term: string): Promise<User | null> {
     return this.userRepository.findOne({
